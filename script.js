f5f62b0a5a50f5e99d36bd116b3293c1d8717188
Hesam <hesamfattahi01@gmail.com>
2022-12-27 23:47:44 +0330

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const inputGroups = [...document.querySelectorAll(".input-group")];
+const inputs = [...document.querySelectorAll("input")];
+const emailInput = document.querySelector("#email");
+
+const errorMessages = [...document.querySelectorAll(".error-message")];
+
+const successMessage = document.querySelector(".success-message");
+
+const btnSubmit = document.querySelector("button");
+
+let emailRegex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+
+btnSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  inputs.forEach((input, i) => {
+    if (!input.value) {
+      inputGroups[i].classList.add("error");
+      input.value = "";
+    } else if (!emailRegex.test(emailInput.value)) {
+      errorMessages[2].textContent = "Looks like this is not an email";
+      inputGroups[2].classList.add("error");
+    } else {
+      inputGroups[i].classList.remove("error");
+      successMessage.classList.add("active");
+      setTimeout(() => {
+        successMessage.classList.remove("active");
+      }, 2000);
+    }
+  });
+});
